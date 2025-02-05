c09f3e973e86f60ee85f87104bd1c22c49be3d8e
Jezz Lucena <jezzlucena@Jezzs-MacBook-Pro.local>
2025-02-04 20:45:06 -0800

    First commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-[100%] max-w-[1024px] mx-auto px-[50px]">
+    <TopBar />
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
---
+++ components/TopBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink to="/">Home</NuxtLink>
+  <NuxtLink to="/about">About</NuxtLink>
+  <a href="/files/JezzLucenaResume2024.pdf" target="_blank">Resumé</a>
+</template>
+
+<style scoped>
+a.router-link-active {
+  color: gray;
+}
+</style>
