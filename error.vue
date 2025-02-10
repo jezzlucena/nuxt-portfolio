19005ac6f4d36a0c90e5fe7a0053c9e0e1f1c6ed
Jezz Lucena <jezzlucena@Jezzs-MacBook-Pro.local>
2025-02-09 19:43:14 -0800

    Add custom error handling page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <div class="bg-black pt-[60px]">
+    <TopBar />
+
+    <div class="bg-white">
+      <div class="w-[100%] max-w-[1024px] mx-auto p-[50px]">
+        <h1>Error {{ error?.statusCode }}</h1>
+        <div>{{ error?.statusMessage }}</div>
+        <br/>
+        <NuxtLink to="/"><Button>Go back home</Button></NuxtLink>
+      </div>
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 40px;
+  font-family: myriad-cond;
+  letter-spacing: 3px;
+}
+</style>
